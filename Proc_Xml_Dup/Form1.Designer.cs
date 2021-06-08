36b700e6a39635b39b74ec7ff58b1140fff5cced
felipe machado <lipizinho-isantos@hotmail.com>
2021-06-08 09:06:48 -0300

    Adicionar arquivos de projeto.

---
+++ Proc_Xml_Dup/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Proc_Xml_Dup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Iniciar = new System.Windows.Forms.Button();
+            this.Txt_Arq = new System.Windows.Forms.TextBox();
+            this.Txt_Caminho = new System.Windows.Forms.TextBox();
+            this.Txt_Arq_dup = new System.Windows.Forms.TextBox();
+            this.Lbl_1 = new System.Windows.Forms.Label();
+            this.Lbl_2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn_Iniciar
+            // 
+            this.Btn_Iniciar.Location = new System.Drawing.Point(12, 24);
+            this.Btn_Iniciar.Name = "Btn_Iniciar";
+            this.Btn_Iniciar.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Iniciar.TabIndex = 0;
+            this.Btn_Iniciar.Text = "Pesquisar";
+            this.Btn_Iniciar.UseVisualStyleBackColor = true;
+            this.Btn_Iniciar.Click += new System.EventHandler(this.Btn_Iniciar_Click);
+            // 
+            // Txt_Arq
+            // 
+            this.Txt_Arq.Location = new System.Drawing.Point(12, 107);
+            this.Txt_Arq.Multiline = true;
+            this.Txt_Arq.Name = "Txt_Arq";
+            this.Txt_Arq.Size = new System.Drawing.Size(386, 321);
+            this.Txt_Arq.TabIndex = 1;
+            // 
+            // Txt_Caminho
+            // 
+            this.Txt_Caminho.Location = new System.Drawing.Point(12, 64);
+            this.Txt_Caminho.Name = "Txt_Caminho";
+            this.Txt_Caminho.Size = new System.Drawing.Size(349, 20);
+            this.Txt_Caminho.TabIndex = 2;
+            // 
+            // Txt_Arq_dup
+            // 
+            this.Txt_Arq_dup.Location = new System.Drawing.Point(404, 107);
+            this.Txt_Arq_dup.Multiline = true;
+            this.Txt_Arq_dup.Name = "Txt_Arq_dup";
+            this.Txt_Arq_dup.Size = new System.Drawing.Size(534, 321);
+            this.Txt_Arq_dup.TabIndex = 3;
+            // 
+            // Lbl_1
+            // 
+            this.Lbl_1.AutoSize = true;
+            this.Lbl_1.Location = new System.Drawing.Point(16, 91);
+            this.Lbl_1.Name = "Lbl_1";
+            this.Lbl_1.Size = new System.Drawing.Size(88, 13);
+            this.Lbl_1.TabIndex = 4;
+            this.Lbl_1.Text = "Lista de Arquivos";
+            // 
+            // Lbl_2
+            // 
+            this.Lbl_2.AutoSize = true;
+            this.Lbl_2.Location = new System.Drawing.Point(401, 91);
+            this.Lbl_2.Name = "Lbl_2";
+            this.Lbl_2.Size = new System.Drawing.Size(100, 13);
+            this.Lbl_2.TabIndex = 5;
+            this.Lbl_2.Text = "Lista de Duplicados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 450);
+            this.Controls.Add(this.Lbl_2);
+            this.Controls.Add(this.Lbl_1);
+            this.Controls.Add(this.Txt_Arq_dup);
+            this.Controls.Add(this.Txt_Caminho);
+            this.Controls.Add(this.Txt_Arq);
+            this.Controls.Add(this.Btn_Iniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Iniciar;
+        private System.Windows.Forms.TextBox Txt_Arq;
+        private System.Windows.Forms.TextBox Txt_Caminho;
+        private System.Windows.Forms.TextBox Txt_Arq_dup;
+        private System.Windows.Forms.Label Lbl_1;
+        private System.Windows.Forms.Label Lbl_2;
+    }
+}
+
---
+++ Proc_Xml_Dup/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace Proc_Xml_Dup
+{
+    public partial class Form1 : Form
+    {
+
+        string Global_Patch;
+        public Form1()
+        {
+            InitializeComponent();
+            DateTime dt_now = DateTime.Now;
+            Global_Patch = @"\\servidor\base\Nfe_Saidas\2021\" + dt_now.ToString("MM_yyy") + @"\XML";
+        }
+
+
+        private void Btn_Iniciar_Click(object sender, EventArgs e)
+        {
+            if (Txt_Caminho.Text.Length > 0)
+            {
+                Find_Dup();
+            }
+            else
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                fbd.Description = "Procurar Pasta de Saidas";
+
+                fbd.RootFolder = Environment.SpecialFolder.Desktop;
+                DateTime date = new DateTime();
+                date = DateTime.Now;
+                fbd.SelectedPath = Global_Patch;
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    Txt_Caminho.Text = fbd.SelectedPath;
+                    Find_Dup();
+                }
+                else
+                    MessageBox.Show("Selecione uma Pasta para Continuar", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            Btn_Iniciar.Enabled = true;
+        }
+        public void Find_Dup()
+        {
+            Btn_Iniciar.Enabled = false;
+            Lbl_1.Text = "Lista de Arquivos";
+            Lbl_2.Text = "Lista de Duplicados";
+            Txt_Arq.Text = null;
+            Txt_Arq_dup.Text = null;
+            DirectoryInfo dir = new DirectoryInfo(Txt_Caminho.Text);
+            if (dir.Exists)
+            {
+                FileInfo[] Arquivos = dir.GetFiles("*.xml");
+                List<FileInfo> Lista_Info = new List<FileInfo>();
+                List<FileInfo> Lista_Info_Dups = new List<FileInfo>();
+
+                foreach (FileInfo file in Arquivos)
+                {
+                    if (file.Name.Length > 27)
+                    {
+                        Txt_Arq.Text += file.Name + Environment.NewLine;
+                        Lista_Info.Add(file);
+                    }
+                }
+
+                foreach (FileInfo list in Lista_Info)
+                {
+                    int count = 0;
+                    foreach (FileInfo list1 in Lista_Info)
+                        if (list1.Name.Substring(22, 12) == list.Name.Substring(22, 12))
+                            count++;
+                    if (count >= 2)
+                    {
+                        Lista_Info_Dups.Add(list);
+
+                        string X_Motivo = string.Empty;
+                        try
+                        {   //Lê o arquivo xml e pega a chave dele 
+                            using (XmlReader reader = XmlReader.Create(list.FullName))
+                            {
+                                while (reader.Read())
+                                {
+                                    if (reader.IsStartElement())
+                                    {
+                                        switch (reader.Name.ToString())
+                                        {
+                                            case "xMotivo":
+                                                X_Motivo = " - " + reader.ReadString();
+                                                break;
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                        catch (XmlException err)
+                        {
+                            MessageBox.Show("" + err.ToString() + "", "Erro");
+                        }
+                        if (X_Motivo == string.Empty)
+                            Txt_Arq_dup.Text += list.Name + " - Sem Autorização" + Environment.NewLine;
+                        else
+                            Txt_Arq_dup.Text += list.Name + X_Motivo + Environment.NewLine;
+                    }
+                    count = 0;
+                }
+                Lbl_1.Text += "   " + Lista_Info.Count;
+                Lbl_2.Text += "   " + Lista_Info_Dups.Count;
+            }
+            else
+                MessageBox.Show("O diretorio Selecionado não existe");
+
+        }
+    }
+}
